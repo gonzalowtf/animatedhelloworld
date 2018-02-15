952abc59e20dc37320bd02646373eb10346928e4
gonzalowtf77 <gonzalowtf@gmail.com>
2018-02-15 17:53:29 +0000

    second commit, multiple choice for the animation

--- src/hello.js
+++ src/hello.js
@@ -3,7 +3,32 @@
 	var app = angular.module('module1',[]);
 
 app.controller("MainController",['$scope','$http',function($scope,$http,toastr){
-	this.word = "";
+	$scope.word = "";
+	$scope.selected = "";
+	$scope.getStatus = function(){
+		if($scope.word != "" && $scope.selected == "bounce"){
+		   return "animated bounce";
+		}
+		if($scope.word != "" && $scope.selected == "flash"){
+			return "animated flash";
+		}
+		if($scope.word != "" && $scope.selected == "fadeOut"){
+			 return "animated fadeOut";
+		}
+		if($scope.word != "" && $scope.selected == "shake"){
+		 return "animated shake";
+		}
+		if($scope.word != "" && $scope.selected == "swing"){
+			return "animated swing";
+		}
+		if($scope.word != "" && $scope.selected == "tada"){
+			return "animated tada";
+		}
+		if($scope.word != "" && $scope.selected == "hinge"){
+			return "animated hinge";
+		}
+			
+	}
 
 }]);
 	
